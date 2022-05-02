b0ca4fd8e7b3873049e7d6f18ce3831d0995438b
Neha Sen <neha712513@gmail.com>
2022-05-02 17:04:37 +0530

    ok

--- unit5-c4/src/components/Orders.jsx
+++ unit5-c4/src/components/Orders.jsx
@@ -1,55 +1,100 @@
+import { useEffect, useState } from "react";
+
 export const Orders = () => {
-    //  Get all data when admin logs in and populate it
-    // store it in redux
-  
-    
-    return (
+  //  Get all data when admin logs in and populate it
+  // store it in redux
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    Allorders();
+  }, []);
+  const Allorders = async () => {
+    let res = await fetch("http://localhost:8080/orders");
+    let data = await res.json();
+    setOrders(data);
+  };
+
+  const handleSort = (e) => {
+    console.log("data");
+    let d = [...orders].sort((a, b) => {
+      return a[e.target.value] > b[e.target.value]
+        ? 1
+        : a[e.target.value] < b[e.target.value]
+        ? -1
+        : 0;
+    });
+
+    setOrders(d);
+  };
+
+  return (
+    <div>
       <div>
         <div>
-          <div>
-            <select className="controls" name="progress" id="progress">
-              <option value="id">ID</option>
-              <option value="status">Status</option>
-              <option value="cost">Cost</option>
-            </select>
-          </div>
-          <table className="orders">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Problem</th>
-                <th>Client Name</th>
-                <th>Status</th>
-                <th>Cost</th>
-                <th>Change Status</th>
-                <th>Accept</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="orders-row">
-                <td className="id"></td>
-                <td className="problem"></td>
-                <td className="owner"></td>
-                <td className="status"></td>
-                <td className="cost"></td>
-                <td className="change-status">
-                  {/* Show select dropdown only if status is Not Accepted */}
-                  <select className="changeStatus" name="changeStatus">
-                    <option value="Pending">Pending</option>
-                    <option value="In Progress">In Progress</option>
-                    <option value="Done">Done</option>
-                    <option value="Not Accepted">Not Accepted</option>
-                  </select>
-                </td>
-                <td className="accept">
-                  {/* Show this button only if status is Not Accepted */}
-                  {/* on change make request to update it in db, and show changed status in table */}
-                  <button>Accept</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <select
+            className="controls"
+            name="progress"
+            id="progress"
+            onChange={(e) => handleSort(e)}
+          >
+            <option onChange={(e) => handleSort(e)} value="id">
+              ID
+            </option>
+            <option onChange={(e) => handleSort(e)} value="status">
+              Status
+            </option>
+            <option onChange={(e) => handleSort(e)} value="cost">
+              Cost
+            </option>
+          </select>
         </div>
+        <table className="orders">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Problem</th>
+              <th>Client Name</th>
+              <th>Status</th>
+              <th>Cost</th>
+              <th>Change Status</th>
+              <th>Accept</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((e) => {
+              return (
+                <tr key={e.id} className="orders-row">
+                  <td className="id">{e.id}</td>
+                  <td className="problem">{e.problem}</td>
+                  <td className="owner">{e.owner_name}</td>
+                  <td className="status">{e.status}</td>
+                  <td className="cost">{e.cost}</td>
+                  <td className="change-status">
+                    {/* Show select dropdown only if status is Not Accepted */}
+                    <select
+                      className="changeStatus"
+                      name="changeStatus"
+                      value={e.status}
+                    >
+                      <option value="Pending">Pending</option>
+                      <option value="In Progress">In Progress</option>
+                      <option value="Done">Done</option>
+                      <option value="Not Accepted">Not Accepted</option>
+                    </select>
+                  </td>
+                  <td className="accept">
+                    {/* Show this button only if status is Not Accepted */}
+                    {/* on change make request to update it in db, and show changed status in table */}
+                    {e.status == "Not Accepted" ? (
+                      <button>Accept</button>
+                    ) : null}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-    );
-  };
+    </div>
+  );
+};
